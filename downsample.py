e788c18302ccd77ac08f2a5ae16d5dcd0b766e05
Strideradu <dunan00001@hotmail.com>
2016-12-29 21:29:27 -0500

    create python script

---
+++ downsample.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 29 21:28:10 2016
+
+downsample pacbio data by assign each read a probablity based on the read length
+
+@author: Nan
+"""
+
+initial_coverage = 100
+traget_coverage = 15
